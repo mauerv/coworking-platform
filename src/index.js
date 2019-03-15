7a2f7f5c8a7869a364a91236e19ac5eb4d063050
mauerv <maurovelazquez1@gmail.com>
2019-03-15 12:54:13 -0300

    Add user a authentication with email and authorization.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import './index.css';
-import App from './components/App/';
-import * as serviceWorker from './serviceWorker';
-import reducer from './reducers'
 
-const store = createStore(
-	reducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+import App from './components/App/';
+import Firebase, { FirebaseContext } from './components/Firebase'
 
 render(
-	<Provider store={store}>
+	<FirebaseContext.Provider value={new Firebase()}>
 		<App /> 
-	</Provider>,
+	</FirebaseContext.Provider>,
 	document.getElementById('root')
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
