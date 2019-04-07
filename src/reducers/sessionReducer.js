bfabafbf035baed907e9964d6a5226de97957918
mauerv <mauro@osn.global>
2019-04-07 12:01:19 -0300

    Rename reducer function.
    Rename the remaining function to standarize the order of subject/verb in my functions.

--- src/reducers/sessionReducer.js
+++ src/reducers/sessionReducer.js
@@ -7,17 +7,17 @@ const initialState = {
 const sessionReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case AUTH_USER_SET:
-			return applySetAuthUser(state, action)
+			return applyAuthUserSet(state, action)
 		default:
 			return state
 	}
 }
 
-const applySetAuthUser = (state, action) => ({
+const applyAuthUserSet = (state, action) => ({
 	 ...state, 
 	 authUser: action.payload
 })
 
 export default sessionReducer	
 
-export { applySetAuthUser }
+export { applyAuthUserSet }
